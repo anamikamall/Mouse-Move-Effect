ac6bd769f5a1661a09711d060bd2d4714a3c0d1e
Anamika Mall <anamikamall221@gmail.com>
2023-07-31 01:55:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 100; //100px
+
+function shadow(e) {
+    // console.log(e);
+    // const width = hero.offsetWidth;
+    // const height = hero.offsetHeight;
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2)); //to make top left -50
+    const yWalk = Math.round((y / height * walk) - (walk / 2));//and bottom right 50
+
+    // console.log(xWalk, yWalk);
+    // text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+
+    text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
+    // #ff00ff pink
+    // #00ffff cyan
+    // #00ff00 green
+    // #0000ff blue 
+}
+
+hero.addEventListener('mousemove', shadow);
